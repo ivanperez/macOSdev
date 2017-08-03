75883bab6498532acac8999d815a18dd7ef6e086
Ivan Perez <ivan@ivanperez.com>
2017-08-03 13:44:59 +0200

    xcode tools

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+echo "Checking Xcode CLI tools"
+# Only run if the tools are not installed yet
+# To check that try to print the SDK path
+xcode-select -p &> /dev/null
+if [ $? -ne 0 ]; then
+  echo "Xcode CLI tools not found. Installing them..."
+  touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+  PROD=$(softwareupdate -l |
+    grep "\*.*Command Line" |
+    head -n 1 | awk -F"*" '{print $2}' |
+    sed -e 's/^ *//' |
+    tr -d '\n')
+  softwareupdate -i "$PROD" -v;
+else
+  echo "Xcode CLI tools OK"
+fi
