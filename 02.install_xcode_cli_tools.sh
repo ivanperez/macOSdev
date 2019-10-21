018f896acdf2edb8d141f5d4d15978695fae4135
@ivanprz <125472+ivanperez@users.noreply.github.com>
2019-10-21 20:26:25 +0200

    Update 02.install_xcode_cli_tools.sh

--- 02.install_xcode_cli_tools.sh
+++ 02.install_xcode_cli_tools.sh
@@ -8,7 +8,8 @@ if [ $? -ne 0 ]; then
   echo "Xcode CLI tools not found. Installing them..."
   touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
   PROD=$(softwareupdate -l | grep "\*.*Command Line" | head -n 1 | awk -F"*" '{print $2}' | sed -e 's/^ *//' | tr -d '\n')
-  /usr/sbin/softwareupdate --install "$PROD" --verbose;
+  # /usr/sbin/softwareupdate --install "$PROD" --verbose;
+  /usr/sbin/softwareupdate --install "Command Line Tools for Xcode-11.0" --verbose
   rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
 else
   echo "Xcode CLI tools OK"
